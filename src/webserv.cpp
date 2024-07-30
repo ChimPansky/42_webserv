2cbdd962954e6f76492993aacfb5d1b762702cb6
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-07-30 14:08:48 +0200

    feat: added empty main to make project compilable

---
+++ src/webserv.cpp
@@ -0,0 +1,14 @@
+// allowed function:
+// execve, dup, dup2, pipe, strerror, gai_strerror,
+// errno, dup, dup2, fork, socketpair, htons, htonl,
+// ntohs, ntohl, select, poll, epoll (epoll_create, epoll_ctl, epoll_wait),
+// kqueue (kqueue, kevent),
+// socket, accept, listen, send, recv, chdir bind,
+// connect, getaddrinfo, freeaddrinfo, setsockopt,
+// getsockname, getprotobyname, fcntl, close, read,
+// write, waitpid, kill, signal, access, stat, open,
+// opendir, readdir and closedir
+
+int main() {
+	return 0;
+}
