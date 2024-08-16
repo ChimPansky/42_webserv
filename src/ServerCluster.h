72d77fc3d081c27df54d08abfa6300d7e0ba0ced
Vladimir Vilenskii <vilenskii.v@gmail.com>
2024-08-16 12:50:16 +0200

    feat: server cluster holds servers

--- src/ServerCluster.h
+++ src/ServerCluster.h
@@ -4,12 +4,16 @@
 #include <vector>
 
 #include "Config.h"
+#include "Server.h"
+#include "utils/unique_ptr.h"
 
 class ServerCluster {
   public:
     ServerCluster(const Config&);
     void Run();
   private:
+    std::vector<utils::unique_ptr<Server> > _servers;
+    typedef std::vector<utils::unique_ptr<Server> >::const_iterator ServersIt;
     bool _run;
 };
 
