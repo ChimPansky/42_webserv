98a808f336f2b1d25f26e089c72e0926f03b085e
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-08-23 11:55:52 +0200

    feat: create classes for parser and request WIP

--- src/http/HttpParser.h
+++ src/http/HttpParser.h
@@ -1,4 +1,20 @@
 #ifndef WS_HTTP_HTTPPARSER_H
 #define WS_HTTP_HTTPPARSER_H
 
+#include <map>
+#include <string>
+
+namespace http {
+
+class HttpParser {
+  public:
+
+  private:
+
+    std::map<std::string, std::string>  _headers;
+
+};
+
+}
+
 #endif  // WS_HTTP_HTTPPARSER_H
--- src/http/Request.h
+++ src/http/Request.h
@@ -1,9 +1,30 @@
 #ifndef WS_HTTP_REQUEST_H
 #define WS_HTTP_REQUEST_H
 
+#include <map>
+#include <string>
 namespace http {
+enum Method {
+    GET,
+    POST,
+    DELETE
+} enum Version {
+    Ver0_9,
+    Ver1_0,
+    Ver1_1,
+    Ver2,
+    Ver3
+}
 
-class Request {};
+class Request {
+  private:
+    std::string _rawRequest;
+    http::Method _method;
+    std::string _url;  // later: put this in struct/class with path, query string (?). fragment (#)
+    http::Version _version;
+    std::map<std::string, std::string> _headers;
+    std::string _body;
+};
 
 }  // namespace http
 
