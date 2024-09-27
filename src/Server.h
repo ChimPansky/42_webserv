38a416b14da22d51f8b2eb3b3698e54141a228a0
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-09-27 16:01:06 +0200

    feat: basic prototypes in Server class

--- src/Server.h
+++ src/Server.h
@@ -23,6 +23,20 @@ class Server {
 
     const std::string& name() const;
 
+    // chimpansky start
+    // when a client sends a (valid) request, iterate through ServerCluster::servers_ and call Compare() on each server.
+    // if Compare() returns true, call ProcessRequest() on that server (this will return a http::Response) and send the Response back to the client (using ClientSession::ClientWriteCallback::Call()).
+    // Compare examples:
+    // Compare("127.0.0.1:8081", "/")
+    // Compare("localhost", "path/to/file")
+    // Compare("localhost", "path/to/file?query=string")
+    // Question: location is full URI or just path portion of URI?
+    bool Compare(const std::string& host, const std::string& location) const;
+    // Response examples:
+    // http::Response response = server.ProcessRequest(request);
+    http::Response ProcessRequest(const http::Request& request);
+    // chimpansky end
+
   private:
     std::string name_;
 };
