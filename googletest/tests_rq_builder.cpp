7d0ebfc016fc10b358af4ffa2b6f2f874fd30a88
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-09-30 13:57:03 +0200

    feat: add test for comparing RQ-Headers

--- googletest/tests_rq_builder.cpp
+++ googletest/tests_rq_builder.cpp
@@ -6,12 +6,14 @@
 TEST(RQBuilderTest1, Positive) {
     http::RequestBuilder rq1 = http::RequestBuilder();
     rq1.buf().resize(100);
-    std::memcpy(rq1.buf().data(), "GET / HTTP/1.1\r\n", 16);
+    std::string raw_req = "GET /index.html HTTP/1.1\r\nKey: Value\r\nHost:localhost\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n";
+    std::memcpy(rq1.buf().data(), raw_req.data(), raw_req.size());
     // maybe fork() and run loop in child to catch possible infinite loop (with timeout)
     while (!rq1.IsReadyForResponse()) {
         rq1.ParseNext();
     }
     EXPECT_EQ(http::HTTP_GET, rq1.rq().method);
+    EXPECT_EQ("localhost", rq1.rq().GetHeaderVal("Host"));
 }
 
 int main (int argc, char **argv) {
