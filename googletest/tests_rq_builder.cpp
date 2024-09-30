9ea9338d55f732d1f5452a34843d8d34078cf454
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-09-30 16:00:14 +0200

    feat: add files as input for rbuilder tests

--- googletest/tests_rq_builder.cpp
+++ googletest/tests_rq_builder.cpp
@@ -1,19 +1,42 @@
 #include <cstring>
 #include "gtest/gtest.h"
 #include "../src/http/RequestBuilder.h"
+#include <fstream>
 
+size_t read_size = 10;
+
+size_t ReadFromFile(std::ifstream& file, std::vector<char>& buf, size_t sz) {
+    size_t old_sz = buf.size();
+    buf.resize(old_sz + sz);
+    file.read(buf.data() + old_sz, sz);
+    return file.gcount();
+}
 //TEST(TestSuiteName, TestName)
 TEST(RQBuilderTest1, Positive) {
-    http::RequestBuilder rq1 = http::RequestBuilder();
-    rq1.buf().resize(100);
-    std::string raw_req = "GET /index.html HTTP/1.1\r\nKey: Value\r\nHost:localhost\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n";
-    std::memcpy(rq1.buf().data(), raw_req.data(), raw_req.size());
-    // maybe fork() and run loop in child to catch possible infinite loop (with timeout)
-    while (!rq1.IsReadyForResponse()) {
-        rq1.ParseNext();
+    http::RequestBuilder rqb = http::RequestBuilder();
+    std::ifstream file("requests/rq1.txt");
+    if (!file.is_open()) {
+        std::cerr << "File not found" << std::endl;
+        return;
     }
-    EXPECT_EQ(http::HTTP_GET, rq1.rq().method);
-    EXPECT_EQ("localhost", rq1.rq().GetHeaderVal("Host"));
+    while (!rqb.IsReadyForResponse() && ReadFromFile(file, rqb.buf(), read_size) > 0) {
+        rqb.ParseNext();
+    }
+    EXPECT_EQ(http::HTTP_GET, rqb.rq().method);
+    EXPECT_EQ("localhost", rqb.rq().GetHeaderVal("Host"));
+}
+
+TEST(RQBuilderTest2, Positive) {
+    http::RequestBuilder rqb = http::RequestBuilder();
+    std::string raw_req = "GET /index.html HTTP/1.1\r\nKey: Value\r\nHost:localhost\r\nUser-Agent: curl/7.68.0\r\nAccept: */*\r\n\r\n";
+    rqb.buf().resize(raw_req.size());
+    std::memcpy(rqb.buf().data(), raw_req.data(), raw_req.size());
+    // maybe fork() and run loop in child to catch possible infinite loop (with timeout)
+    while (!rqb.IsReadyForResponse()) {
+        rqb.ParseNext();
+    }
+    EXPECT_EQ(http::HTTP_GET, rqb.rq().method);
+    EXPECT_EQ("localhost", rqb.rq().GetHeaderVal("Host"));
 }
 
 int main (int argc, char **argv) {
