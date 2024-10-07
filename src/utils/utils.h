729e9feffc2ce768c5daea98880513612877d838
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-10-07 14:57:32 +0200

    feat: restor commented test main in utils/utils.h

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -77,3 +77,41 @@ std::pair<bool /*is_valid*/, NumType> StrToNumericNoThrow(const std::string& str
 }  // namespace utils
 
 #endif  // WS_UTILS_UTILS_H
+// #include <iostream>
+
+// template <typename NumType>
+// void test_one(std::string str) {
+//     std::pair<bool, NumType> res = utils::StrToNumericNoThrow<NumType>(str);
+//     std::cout << str << ": " << res.first << " " << res.second << std::endl;
+// }
+
+// #include <netinet/in.h>
+// int main() {
+//     test_one<int>("123");
+//     test_one<int>("123a");
+//     test_one<int>("-123");
+//     test_one<int>("123.");
+//     test_one<int>("123222222222222222222222222222222222222222");
+//     test_one<int>("-123222222222222222222222222222222222222222");
+
+//     test_one<unsigned short>("     001123");
+//     test_one<unsigned short>("     0");
+//     test_one<in_port_t>("     a0053");
+//     test_one<unsigned short>("     0x1  ");
+//     test_one<unsigned short>("0X1");
+//     test_one<unsigned short>("-0X1");
+//     test_one<unsigned short>("100000");
+
+//     test_one<float>("1f");
+//     test_one<float>("1.0");
+//     test_one<float>("-50000000000000000");
+//     test_one<float>("-5000000.0");
+//     test_one<float>("a50");
+//     test_one<float>("50a");
+//     test_one<float>("a");
+
+//     test_one<bool>("0");
+//     test_one<bool>("100");
+//     test_one<bool>("true");
+
+// }
