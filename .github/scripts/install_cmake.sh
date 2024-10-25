8f5559ec3506eb56c19159ecf66fe89ba46b077e
Vladimir Vilenskii <vilenskii.v@gmail.com>
2024-10-25 20:11:06 +0300

    ci: refact of github actions

---
+++ .github/scripts/install_cmake.sh
@@ -0,0 +1,10 @@
+VERSION=$1
+BUILD=$2
+
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+
+./bootstrap
+make -j$(nproc)
+sudo make install
