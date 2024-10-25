eafcdefefecd7bdd6e2a3a78db1ef814d4209c40
Vladimir Vilenskii <vilenskii.v@gmail.com>
2024-10-25 20:14:59 +0300

    fix: cd

--- .github/scripts/install_cmake.sh
+++ .github/scripts/install_cmake.sh
@@ -1,9 +1,9 @@
 VERSION=$1
 BUILD=$2
 
-wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
-tar -xzvf cmake-$version.$build.tar.gz
-cd cmake-$version.$build/
+wget https://cmake.org/files/v$VERSION/cmake-$VERSION.$BUILD.tar.gz
+tar -xzvf cmake-$VERSION.$BUILD.tar.gz
+cd cmake-$VERSION.$BUILD/
 
 ./bootstrap
 make -j$(nproc)
