381b16c35cd50cd7477e601f76a643e3687d8b08
anastasiia <anastasia.pashkova2003@gmail.com>
2024-11-22 19:33:56 +0100

    feat: ReadFile using ifstream

--- src/server/FileProcessor.h
+++ src/server/FileProcessor.h
@@ -1,38 +1,46 @@
 #ifndef WS_SERVER_FILE_PROCESSOR_H
 #define WS_SERVER_FILE_PROCESSOR_H
 
+#include <ResponseCodes.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <fstream>
+
 #include "AResponseProcessor.h"
-#include <ResponseCodes.h>
 #include <unique_ptr.h>
 
 class FileProcessor : public AResponseProcessor {
   private:
-    std::string ReadFile_();
-    std::string GetContentType(const std::string& file); // return mime type based on extension (mb move to utils later)
+    std::vector<char> ReadFile_(std::ifstream& file)
+    {
+        return std::vector<char>(std::istreambuf_iterator<char>(file),
+                                 std::istreambuf_iterator<char>());
+    }
+    std::string GetContentType(
+        const std::string& file);  // return mime type based on extension (mb move to utils later)
   public:
     // change back to config
     FileProcessor(const std::string& file_path,
                   utils::unique_ptr<http::IResponseCallback> response_rdy_cb)
-        : AResponseProcessor(response_rdy_cb), err_response_processor_(utils::unique_ptr<GeneratedErrorResponseProcessor>(NULL))
+        : AResponseProcessor(
+              response_rdy_cb)  //,
+                                //err_response_processor_(utils::unique_ptr<GeneratedErrorResponseProcessor>(NULL))
     {
         if (access(file_path.c_str(), F_OK) == -1) {
             // return 404
-            err_response_processor_ = utils::unique_ptr<GeneratedErrorResponseProcessor>(new GeneratedErrorResponseProcessor(response_rdy_cb_, http::HTTP_NOT_FOUND));
-            return ;
+            // err_response_processor_ = utils::unique_ptr<GeneratedErrorResponseProcessor>(
+            //     new GeneratedErrorResponseProcessor(response_rdy_cb_, http::HTTP_NOT_FOUND));
+            // return;
         }
-        fd_ = open(file_path.c_str(), O_RDONLY);
-        if (fd_ == -1) {
+        std::ifstream file(file_path.c_str(), std::ios::binary);
+        if (!file.is_open()) {
             // return 50x (for now 500)
-          err_response_processor_ = utils::unique_ptr<GeneratedErrorResponseProcessor>(new GeneratedErrorResponseProcessor(response_rdy_cb_, http::HTTP_NOT_FOUND));
-          return ;
+            // err_response_processor_ = utils::unique_ptr<GeneratedErrorResponseProcessor>(
+            //     new GeneratedErrorResponseProcessor(response_rdy_cb_, http::HTTP_NOT_FOUND));
+            // return;
         }
-        std::string body_str = ReadFile_();
-        std::vector<char> body;
-        body.reserve(body_str.size());
-        std::copy(body_str.begin(), body_str.end(), std::back_inserter(body));
+        std::vector<char> body = ReadFile_(file);
         std::map<std::string, std::string> hdrs;
         hdrs["Content-Type"] = GetContentType(file_path);
         // hdrs["Connection"] = "Closed";
@@ -40,11 +48,10 @@ class FileProcessor : public AResponseProcessor {
         response_rdy_cb_->Call(utils::unique_ptr<http::Response>(
             new http::Response(http::HTTP_OK, http::HTTP_1_1, hdrs, body)));
     }
-    ~FileProcessor() { close(fd_); };
+    ~FileProcessor() {};
 
   private:
-    int fd_;
-    utils::unique_ptr<GeneratedErrorResponseProcessor>  err_response_processor_;
+    utils::unique_ptr<GeneratedErrorResponseProcessor> err_response_processor_;
 };
 
 #endif  // WS_SERVER_FILE_PROCESSOR_H
