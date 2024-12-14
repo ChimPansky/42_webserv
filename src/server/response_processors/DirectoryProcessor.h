fd8884cc23952baf4cfd2946396040d589dbded7
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2024-12-14 19:55:38 +0100

    format: git precommit test

--- src/server/response_processors/DirectoryProcessor.h
+++ src/server/response_processors/DirectoryProcessor.h
@@ -1,26 +1,27 @@
 #ifndef WS_SERVER_RESPONSE_PROCESSORS_DIRECTORY_PROCESSOR_H
 #define WS_SERVER_RESPONSE_PROCESSORS_DIRECTORY_PROCESSOR_H
 
+#include <shared_ptr.h>
 #include <unique_ptr.h>
 
-
 #include "AResponseProcessor.h"
-#include "Request.h"
 #include "Location.h"
+#include "Request.h"
 #include "ResponseCodes.h"
-#include <shared_ptr.h>
 
 class DirectoryProcessor : public AResponseProcessor {
   private:
     std::string GetContentType(const std::string& file);
 
-    enum DirEntryType{
+    enum DirEntryType {
         DE_FILE,
         DE_FOLDER
     };
     class DirEntry {
       public:
-        DirEntry(const std::string& name, const std::string fpath, DirEntryType type, bool visited) : name_(name), fpath_(fpath), type_(type), visited_(visited) {}
+        DirEntry(const std::string& name, const std::string fpath, DirEntryType type, bool visited)
+            : name_(name), fpath_(fpath), type_(type), visited_(visited)
+        {}
 
         std::string name() { return name_; }
         std::string fpath() { return fpath_; }
@@ -32,13 +33,14 @@ class DirectoryProcessor : public AResponseProcessor {
         std::string fpath_;
         DirEntryType type_;
         bool visited_;
-        // change_date,...
+        // change_date,..
     };
 
   public:
     DirectoryProcessor(const std::string& file_path,
-                  utils::unique_ptr<http::IResponseCallback> response_rdy_cb, const http::Request& rq, utils::shared_ptr<Location> loc);
-    ~DirectoryProcessor() {};
+                       utils::unique_ptr<http::IResponseCallback> response_rdy_cb,
+                       const http::Request& rq, utils::shared_ptr<Location> loc);
+    ~DirectoryProcessor(){};
 
   private:
     utils::unique_ptr<GeneratedErrorResponseProcessor> err_response_processor_;
@@ -46,7 +48,8 @@ class DirectoryProcessor : public AResponseProcessor {
     bool ListDirectory_(const std::string& path);
     std::vector<DirEntry> entries_;
 
-    std::pair<bool /*success*/, std::vector<DirEntry> /*dir_entries*/> GetDirEntries_(const char* directory);
+    std::pair<bool /*success*/, std::vector<DirEntry> /*dir_entries*/> GetDirEntries_(
+        const char* directory);
 };
 
 #endif  // WS_SERVER_RESPONSE_PROCESSORS_DIRECTORY_PROCESSOR_H
