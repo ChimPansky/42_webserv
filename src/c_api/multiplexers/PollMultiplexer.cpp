991f378e8cb719ef9fc425d93f4d272071caf1e7
anastasiia <anastasia.pashkova2003@gmail.com>
2024-12-19 19:14:39 +0100

    fix: small fix in poll and select multiplexers

--- src/c_api/multiplexers/PollMultiplexer.cpp
+++ src/c_api/multiplexers/PollMultiplexer.cpp
@@ -11,33 +11,35 @@ namespace c_api {
 int PollMultiplexer::CheckOnce(const FdToCallbackMap& rd_sockets, const FdToCallbackMap& wr_sockets)
 {
     std::vector<struct pollfd> fds;
-    nfds_t nb_of_fds;
 
     for (FdToCallbackMapIt it = rd_sockets.begin(); it != rd_sockets.end(); ++it) {
-        struct pollfd fd_struct = {it->first, POLLIN, 0};
+        struct pollfd fd_struct = {it->first, POLLIN, /*revents*/ 0};
         fds.push_back(fd_struct);
     }
     for (FdToCallbackMapIt it = wr_sockets.begin(); it != wr_sockets.end(); ++it) {
-        struct pollfd fd_struct = {it->first, POLLOUT, 0};
+        struct pollfd fd_struct = {it->first, POLLOUT, /*revents*/ 0};
         fds.push_back(fd_struct);
     }
 
-    nb_of_fds = fds.size();
-    int timeout = 10000;  // ms, or 10 sec
+    int timeout_ms = 10000;  // 10s
 
-    int num_of_fds = poll(fds.data(), nb_of_fds, timeout);
+    int num_of_fds = poll(fds.data(), (nfds_t)fds.size(), timeout_ms);
     if (num_of_fds < 0) {
         LOG(ERROR) << "poll failed: " << std::strerror(errno);
         return 1;
     }
     for (std::vector<struct pollfd>::iterator it = fds.begin(); it != fds.end(); ++it) {
-        FdToCallbackMapIt sockets_it = rd_sockets.find(it->fd);
-        if ((it->revents & POLLIN) && sockets_it != rd_sockets.end()) {
-            sockets_it->second->Call(sockets_it->first);
+        if (it->revents & POLLIN) {
+            FdToCallbackMapIt sockets_it = rd_sockets.find(it->fd);
+            if (sockets_it != rd_sockets.end()) {
+                sockets_it->second->Call(sockets_it->first);
+            }
         }
-        sockets_it = wr_sockets.find(it->fd);
-        if ((it->revents & POLLOUT) && sockets_it != wr_sockets.end()) {
-            sockets_it->second->Call(sockets_it->first);
+        if (it->revents & POLLOUT) {
+            FdToCallbackMapIt sockets_it = wr_sockets.find(it->fd);
+            if (sockets_it != wr_sockets.end()) {
+                sockets_it->second->Call(sockets_it->first);
+            }
         }
     }
     return 0;
