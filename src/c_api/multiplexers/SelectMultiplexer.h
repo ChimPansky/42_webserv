a86926fd44aa2f029280a715a65cb0fd3ee93e92
Vladimir Vilenskii <vvilensk@c1r2p10.42vienna.com>
2024-12-19 23:11:07 +0100

    fix: include

--- src/c_api/multiplexers/SelectMultiplexer.h
+++ src/c_api/multiplexers/SelectMultiplexer.h
@@ -1,6 +1,8 @@
 #ifndef WS_C_API_MULTIPLEXERS_SELECT_MULTIPLEXER_H
 #define WS_C_API_MULTIPLEXERS_SELECT_MULTIPLEXER_H
 
+#include <sys/time.h>
+
 #include "IMultiplexer.h"
 
 namespace c_api {
@@ -11,7 +13,7 @@ class SelectMultiplexer : public IMultiplexer {
     void CheckOnce(const FdToCallbackMap& rd_sockets, const FdToCallbackMap& wr_sockets);
 
   private:
-    struct timeval* __restrict timeout_;
+    struct timeval* timeout_;
     struct timeval timeout_descr_;
     static int kMaxSelectFds_() { return 1023; }
 };
