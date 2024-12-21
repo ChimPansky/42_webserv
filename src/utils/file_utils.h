1959aaef777a1cc225dad61785a32f532cf38307
Vladimir Vilenskii <vvilensk@c1r2p10.42vienna.com>
2024-12-21 22:08:01 +0100

    fix: inf loop

--- src/utils/file_utils.h
+++ src/utils/file_utils.h
@@ -46,7 +46,7 @@ std::pair<bool, std::string> CreateAndOpenTmpFileToStream(FileStream &fs)
     std::string tmp_name;
     do {
         tmp_name = TMP_DIR + GenerateRandomString(TMP_FILE_NAME_LEN);
-    } while (access(tmp_name.c_str(), F_OK) != 0);
+    } while (access(tmp_name.c_str(), F_OK) == 0);
     // Create and use the file
     fs.open(tmp_name.c_str());
     return std::make_pair(fs.is_open(), tmp_name);
