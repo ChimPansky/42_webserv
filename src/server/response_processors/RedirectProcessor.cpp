96572b32b15fd4718dde77d852705c32b238169c
Thomas Kasbarian <33182451+ChimPansky@users.noreply.github.com>
2024-12-26 14:24:40 +0100

    feat: never close

--- src/server/response_processors/RedirectProcessor.cpp
+++ src/server/response_processors/RedirectProcessor.cpp
@@ -8,8 +8,6 @@ RedirectProcessor::RedirectProcessor(RequestDestination dest,
     : AResponseProcessor(dest, response_rdy_cb)
 {
     std::map<std::string, std::string> hdrs;
-    // TODO: close only if redir to outer resource
-    hdrs["Connection"] = "Close";
     hdrs["Location"] = redirect_uri;
     response_rdy_cb_->Call(utils::unique_ptr<http::Response>(
         new http::Response(code, http::HTTP_1_1, hdrs, std::vector<char>())));
